cba4f3f18bc9c0731c1fef6eb051fef7d8e93a04
Kaiya Park <Kaiya.Park@gmail.com>
2019-01-11 23:23:55 -0800

    Finshed Project One

--- FRC_Project_01/FrcProject_01/src/main/java/frc/robot/Motors.java
+++ FRC_Project_01/FrcProject_01/src/main/java/frc/robot/Motors.java
@@ -4,5 +4,6 @@ import edu.wpi.first.wpilibj.Spark;
 
 public class Motors 
 {
-
+    public  Spark leftMotor = new Spark(2);
+	public  Spark rightMotor = new Spark(1);
 }
--- FRC_Project_01/FrcProject_01/src/main/java/frc/robot/Robot.java
+++ FRC_Project_01/FrcProject_01/src/main/java/frc/robot/Robot.java
@@ -1,22 +1,29 @@
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
 
 public class Robot extends IterativeRobot 
 {
+	Motors motorObject;
+	Joysticks joystick;
+	Boolean joysick;
 	//This function is run whenever the robot starts. This function is used for any initialization of code
 	@Override
 	public void robotInit() 
 	{
-		
+		joystick = new Joysticks();
+		motorObject = new Motors();
+		joysick = true;
+
 	}
 
 	 //This function runs once, right before autonomous period starts. 
 	@Override
 	public void autonomousInit() 
 	{
-
+		motorObject.leftMotor.set(.5);
+		motorObject.rightMotor.set(-.5);
 	}
 
 	//This is the function that is called during the autonomous period
@@ -27,12 +34,30 @@ public class Robot extends IterativeRobot
 		
 	}
 
+	@Override
+	public void teleopInit() 
+	{
+		motorObject.leftMotor.set(.5);
+		motorObject.rightMotor.set(-.5);
+	}
 	//This is the function that is called during the Tele-operated period
 	//This function runs periodically, meaning it acts as an infinite loop
 	@Override
 	public void teleopPeriodic() 
 	{
-
+		joystick.updateMainController();
+		if (joystick.buttonA){
+			if(joysick){
+		motorObject.leftMotor.set(-.5);
+		motorObject.rightMotor.set(.5);
+		joysick = false;
+			}
+			else{
+				motorObject.leftMotor.set(.5);
+				motorObject.rightMotor.set(-.5);
+				joysick = true;
+					}
+	}
 	}
 
 	//This is the function that is called during the test
